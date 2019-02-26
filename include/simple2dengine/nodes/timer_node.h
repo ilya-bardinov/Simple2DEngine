49d78ea241a539316b81860cd1868b7109bc0121
Ilya Bardinov <ilya.bardinov@gmail.com>
2019-02-26 16:52:46 +0200

    add timer node

---
+++ include/simple2dengine/nodes/timer_node.h
@@ -0,0 +1,114 @@
+/**
+ * @file timer_node.h
+ * @author Ilya Bardinov (ilya.bardinov@gmail.com)
+ * @brief Timer Node.
+ * @date 2019-02-26
+ *
+ * @copyright Copyright (c) 2019
+ *
+ */
+
+#ifndef _SIMPLE2DENGINE_NODES_TIMER_NODE_H_
+#define _SIMPLE2DENGINE_NODES_TIMER_NODE_H_
+
+#include <string>
+#include <functional>
+
+#include "simple2dengine/engine.h"
+#include "simple2dengine/nodes/node.h"
+
+namespace simple2dengine
+{
+    /**
+     * @brief Timer Node.
+     * Used to set timer and execute command on timer finish.
+     *
+     */
+    class TimerNode : public Node
+    {
+      public:
+        /**
+         * @brief Construct a new Time Node.
+         *
+         * @param engineRef reference to Engine object
+         * @param nodeName name of the node.
+         *
+         * @see Engine.
+         * @see Node.
+         */
+        TimerNode(Engine& engineRef,
+                const std::string& nodeName,
+                unsigned int time = 0,
+                bool isOneShot = true) : Node(engineRef, nodeName), finishTime(time), oneShot(isOneShot) { };
+        /**
+         * @brief Set finish time.
+         *
+         * @param time - time until onTimeout will be called.
+         */
+        void setTime(unsigned int time);
+        /**
+         * @brief Start timer.
+         *
+         */
+        void start();
+        /**
+         * @brief Pause timer.
+         *
+         */
+        void pause();
+        /**
+         * @brief Reset timer to 0.
+         *
+         */
+        void reset();
+        /**
+         * @brief Check if timer is paused.
+         *
+         * @return bool pause or not.
+         */
+        bool isPaused() const;
+        /**
+         * @brief Set timer to one shot.
+         *
+         * @param oneShot if true - one shot timer.
+         */
+        void setOneShot(bool oneShot);
+        /**
+         * @brief Check if timer is one shot.
+         *
+         * @return bool one shot or not.
+         */
+        bool isOneShot() const;
+        /**
+         * @brief Store a function that will be called when timeout happens.
+         * For example you can use it like this:
+         *
+         * timer->onTimeout([this] () {
+         *     sound->stop();
+         *     text->setText("Lose!");
+         * });
+         *
+         * @param function lambda function
+         */
+        void onTimeout(std::function<void()> function);
+
+      protected:
+        /**
+         * @brief Override base update().
+         * We need to calculate.
+         *
+         * @see Node.
+         */
+        virtual void update(unsigned int deltaInMs);
+
+      private:
+        unsigned int finishTime = 0; // amount of time need to send finish signal
+        unsigned int elapsedTime = 0; // current elapsed time
+        bool oneShot = true; // if true - timer will not restart
+        bool paused = false; // if pause if true - time will not elapse
+
+        std::function<void()> timeoutFunc; // signal on timeout
+    };
+} // simple2dengine
+
+#endif // _SIMPLE2DENGINE_NODES_TIMER_NODE_H_
---
+++ src/nodes/timer_node.cpp
@@ -0,0 +1,67 @@
+#include "simple2dengine/nodes/timer_node.h"
+
+namespace simple2dengine
+{
+    void TimerNode::setTime(unsigned int time)
+    {
+        finishTime = time;
+    }
+
+    void TimerNode::start()
+    {
+        paused = false;
+    }
+
+    void TimerNode::pause()
+    {
+        paused = true;
+    }
+
+    void TimerNode::reset()
+    {
+        elapsedTime = 0;
+    }
+
+    bool TimerNode::isPaused() const
+    {
+        return paused;
+    }
+
+    void TimerNode::setOneShot(bool _oneShot)
+    {
+        this->oneShot = _oneShot;
+    }
+
+    bool TimerNode::isOneShot() const
+    {
+        return oneShot;
+    }
+
+    void TimerNode::onTimeout(std::function<void()> function)
+    {
+        timeoutFunc = std::move(function);
+    }
+
+    void TimerNode::update(unsigned int deltaInMs)
+    {
+        if(!paused && finishTime > 0)
+        {
+            if(elapsedTime < finishTime)
+            {
+                elapsedTime += deltaInMs;
+            }
+            else
+            {
+                if(oneShot)
+                {
+                    paused = true;
+                }
+                elapsedTime = 0;
+
+                timeoutFunc();
+            }
+        }
+
+        Node::update(deltaInMs);
+    }
+} // simple2dengine
